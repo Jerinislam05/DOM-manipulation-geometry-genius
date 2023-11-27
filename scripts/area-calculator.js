ef588d88236e1d055665f3b1d875370a2c7add6d
Jerinislam05 <jerinislam05@gmail.com>
2023-11-27 20:01:23 +0000

    completed triangle area

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -4,4 +4,17 @@ function calculateTriangleArea() {
     const baseValueText = baseField.value;
     const base = parseFloat(baseValueText);
     console.log(base);
+
+    // get triangle hight value
+    const hightField = document.getElementById('triangle-hight');
+    const hightValueText = hightField.value;
+    const hight = parseFloat(hightValueText);
+    console.log(hight);
+
+    const area = 0.5 * base * hight;
+    console.log(area);
+
+    // show triangle area
+    const areaSpan = document.getElementById('triangle-area');
+    areaSpan.innerText = area;
 }

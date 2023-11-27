6f1f3caf3bf1aeb5b051d51a148c960440dfe13a
Jerinislam05 <jerinislam05@gmail.com>
2023-11-27 19:28:57 +0000

    added event handler

---
+++ scripts/area-calculator.js
@@ -0,0 +1,7 @@
+function calculateTriangleArea() {
+    // get triangle base value
+    const baseField = document.getElementById('triangle-base');
+    const baseValueText = baseField.value;
+    const base = parseFloat(baseValueText);
+    console.log(base);
+}

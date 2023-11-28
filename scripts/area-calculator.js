cb0b56ad8770b2c2e99df124ee41cc64317af57a
Jerinislam05 <jerinislam05@gmail.com>
2023-11-28 20:37:02 +0000

    power of reusable code

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -51,6 +51,16 @@ function calculateParallelogramArea() {
     setElementInnerText('Parallelogram-area', area);
 }
 
+
+function calculateEllipseArea() {
+    const majorRadius = getInputValue('ellipse-major radius');
+    const minorRadius = getInputValue('ellipse-minor radius');
+    const area = 3.14 * majorRadius * minorRadius;
+    setElementInnerText('ellipse-area', area)
+}
+
+
+// reusable get input value field in number
 function getInputValue(fieldId) {
     const inputField = document.getElementById(fieldId);
     const inputValueText = inputField.value;

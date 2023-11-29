227cafb0d69e35c3d9a5ceb2140a3ed9fc08a885
Jerinislam05 <jerinislam05@gmail.com>
2023-11-29 19:00:26 +0000

    create dynamic html

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -48,16 +48,19 @@ function calculateRectangleArea() {
 
 // reuseable function --> reduce duplicate code
 function calculateParallelogramArea() {
-    const base = getInputValue('Parallelogram-base');
-    const height = getInputValue('Parallelogram-height');
-    
+    const base = getInputValue('parallelogram-base');
+    const height = getInputValue('parallelogram-height');
+
     // validate
-    if (isNaN(base) || isNaN(height)) {} {
+    if (isNaN(base) || isNaN(height)) {
         alert('Please insert number');
         return
     }
     const area = base * height;
-    setElementInnerText('Parallelogram-area', area);
+    setElementInnerText('parallelogram-area', area);
+
+    // add to calculation entry
+    addToCalculationEntry('parallelogram', area)
 }
 
 
@@ -83,6 +86,25 @@ function setElementInnerText(elementId, area) {
     const element = document.getElementById(elementId);
     element.innerText = area;
 }
+
+
+// add to calculation entry
+/* 
+1. get the element where you want to add the dynamic HTML 
+2. create an element you want to add 
+3. if needed add some class
+4. set inner HTML. it could be dynamic Template string
+5. append the created element as a child of the parent
+*/
+function addToCalculationEntry(areaType, area) {
+    console.log(areaType + ' ' + area);
+    const calculationEntry = document.getElementById('calculation-entry')
+    const p = document.createElement('p');
+    p.innerHTML = areaType+ ' ' +area;
+    calculationEntry.appendChild(p);
+
+}
+
 
 /* data validation 
 1. set the proper type of the input field(number, data, email)
